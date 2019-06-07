c42929dce7e18d1766f4452df53f8155d5da717b
havuong <havuong138@gmail.com>
2019-06-07 18:44:54 +0700

    Reusable Response Specifications

--- src/test/java/SpecBuilderDemo.java
+++ src/test/java/SpecBuilderDemo.java
@@ -1,17 +1,31 @@
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class SpecBuilderDemo {
     private static RequestSpecification reqSpec;
+    private static ResponseSpecification resSpec;
 
     @BeforeClass
     public static void createRequestSpecification() {
         reqSpec = new RequestSpecBuilder().
                 setBaseUri("http://api.zippopotam.us").
+                build();
+    }
+
+    @BeforeClass
+    public static void createResponseSpecification() {
+
+        resSpec = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
                 build();
     }
 
@@ -25,5 +39,19 @@ public class SpecBuilderDemo {
                 then().
                 assertThat().
                 statusCode(200);
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills_withResponseSpec() {
+
+        given().
+                spec(reqSpec).
+                when().
+                get("us/90210").
+                then().
+                spec(resSpec).
+                and().
+                assertThat().
+                body("places[0].'place name'", equalTo("Beverly Hills"));
     }
 }
