48bceed1d4ea1fe53c3858a1456638858316ee4c
havuong <havuong138@gmail.com>
2019-06-07 17:32:18 +0700

    Set BaseURI with RestAssured.baseURI for each test

--- src/test/java/CheckResponseBody.java
+++ src/test/java/CheckResponseBody.java
@@ -1,15 +1,24 @@
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class CheckResponseBody {
+
+    @Before
+    public void setBaseUri() {
+
+        RestAssured.baseURI = "http://zippopotam.us/us/90210";
+    }
+    
     @Test
     public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
 
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("").
                 then().
                 assertThat().
                 body("places[0].'place name'", equalTo("Beverly Hills"));
@@ -20,7 +29,7 @@ public class CheckResponseBody {
 
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("").
                 then().
                 assertThat().
                 body("places[0].state", equalTo("California"));
@@ -31,7 +40,7 @@ public class CheckResponseBody {
 
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("").
                 then().
                 assertThat().
                 body("places.'place name'", hasItem("Beverly Hills"));
@@ -42,7 +51,7 @@ public class CheckResponseBody {
 
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("").
                 then().
                 assertThat().
                 body("places.'place name'", hasSize(1));
--- src/test/java/DataProviderDemo.java
+++ src/test/java/DataProviderDemo.java
@@ -9,7 +9,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class DataProviderDemo {
 
     @DataProvider
-    public static Object[][] zipCodesAndPlaces() {
+    public static Object[] zipCodesAndPlaces() {
         return new Object[][]{
                 { "us", "90210", "Beverly Hills" },
                 { "us", "12345", "Schenectady" },
